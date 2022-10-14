eb1f48a0132d589dd064d2b3725a40c36b954935
Fatemeh Rahimi <fa.rahimi5475@gmail.com>
2022-10-14 16:56:28 +0100

    level 200

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 //You can edit ALL of the code here
+const searchBox = document.getElementById("search-box");
+const searchCount = document.getElementById("search-count");
+// let optionTag = document.createElement("option");
+// let listTag = document.getElementById("list");
+
 function setup() {
+  makePageForEpisodes();
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
-  makePageForEpisodes();
+  //event listener
+  searchBox.addEventListener("keyup", onSearchKeyUp);
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 function makePageForEpisodes() {
   let allEpisodes = getAllEpisodes();
   let rootDiv = document.getElementById("root");
+function makePageForEpisodes(episodeList) {
   let allEpisodeDiv = document.getElementById("all-episodesDiv");
+  allEpisodeDiv.innerHTML = "";
 
   allEpisodes.forEach((e) => {
+  episodeList.forEach((e) => {
     let eachEpisodeDiv = document.createElement("div");
     eachEpisodeDiv.setAttribute("class", "episode-div");
     let headerTag = document.createElement("h3");
@@ -21,6 +28,7 @@ function makePageForEpisodes() {
     let summaryText = document.createElement("p");
 
     //zero-padded to two digits
+    //header - zero-padded to two digits
     headerTag.innerText =
       e.number > 9
         ? `${e.name} - S0${e.season}E${e.number}`
@@ -31,12 +39,30 @@ function makePageForEpisodes() {
     text = text.replaceAll("<p>", "");
     text = text.replaceAll("</p>", "");
     summaryText.innerText = text;
-
     eachEpisodeDiv.append(headerTag, imageTag, summaryText);
     allEpisodeDiv.append(eachEpisodeDiv);
   });
 }
 makePageForEpisodes(getAllEpisodes());
 
-window.onload = setup;
 //window.onload = setup();
+function onSearchKeyUp(event) {
+  const searchTerm = event.target.value.toLowerCase();
+  const allEpisodes = getAllEpisodes();
+  const filteredEpisodes = allEpisodes.filter((e) => {
+
+    const episodeName = e.name.toLowerCase();
+    const episodeSummary = e.summary.toLowerCase();
+    return (
+      episodeName.includes(searchTerm) || episodeSummary.includes(searchTerm)
+    );
+
+  });
+  const filteredCount = filteredEpisodes.length;
+  const allCount = allEpisodes.length;
+  const countString = `Displaying ${filteredCount} / ${allCount}`;
+  searchCount.innerText = countString;
+  makePageForEpisodes(filteredEpisodes);
+}
+
+window.onload = setup;
