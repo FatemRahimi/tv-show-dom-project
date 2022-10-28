69d5bca1aab8ae6c3200917f1db106e87423f722
Fatemeh Rahimi <fa.rahimi5475@gmail.com>
2022-10-28 02:50:50 +0100

    css code

--- script.js
+++ script.js
@@ -7,21 +7,25 @@ const showList = document.getElementById("show-list");
 let selectEpisodeTag = document.getElementById("selectEpisode");
 let allEpisodesDiv = document.getElementById("all-episodesDiv");
 //search
+
 const searchShow = document.getElementById("searchShow"); //input search for shows
 const showCount = document.getElementById("show-count"); // shows counter
 const searchEpisode = document.getElementById("searchBox"); // input search for episode
 const episodeCount = document.getElementById("search-count"); // episode counter
 const backToShows = document.getElementById("back-toshows");
+
 let showsList = getAllShows();
 function setup() {
   selectOptionForShows(showsList);
   makePageForShows(showsList);
+
   backToShows.addEventListener("click", goBackToAllShows);
   searchShow.addEventListener("keyup", onSearchKeyUpShow);
   searchEpisode.addEventListener("keyup", onSearchKeyUpEpisode);
   selectEpisodeTag.addEventListener("change", getSelectedEpisodeValue);
   selectShowTag.addEventListener("change", getSelectedShowValue);
 }
+
 function selectOptionForShows(shows) {
   selectShowTag.innerHTML = "";
   // sorting list in order
@@ -34,6 +38,7 @@ function selectOptionForShows(shows) {
       return 0;
     }
   });
+
   selectAllShows.innerText = "View All Shows";
   selectAllShows.value = "All-Shows";
   selectShowTag.appendChild(selectAllShows);
@@ -44,14 +49,17 @@ function selectOptionForShows(shows) {
     selectShowTag.append(showOptionTag);
   });
 }
+
 function selectOptionForEpisodes(episodeList) {
   selectEpisodeTag.innerHTML = "";
   const selectAllEpisodes = document.createElement("option");
   selectAllEpisodes.innerText = "View All Episodes";
   selectAllEpisodes.value = "All-Episode";
   selectEpisodeTag.appendChild(selectAllEpisodes);
+
   episodeList.forEach((e) => {
     let optionTag = document.createElement("option");
+
     let dropdownList = `${formatSeriesAndEpisode(e.season, e.number)} - ${
       e.name
     }`;
@@ -60,11 +68,14 @@ function selectOptionForEpisodes(episodeList) {
     selectEpisodeTag.append(optionTag);
   });
 }
+
 function formatSeriesAndEpisode(season, number) {
   return number > 9 ? `S0${season}E${number}` : `S0${season}E0${number}`;
 }
+
 function makePageForEpisodes(episodeList) {
   allEpisodesDiv.innerHTML = "";
+
   episodeList.forEach((e) => {
     backToShows.style.display = "block";
     let eachEpisodeDiv = document.createElement("div");
@@ -72,20 +83,24 @@ function makePageForEpisodes(episodeList) {
     let headerTag = document.createElement("h3");
     let imageTag = document.createElement("img");
     let summaryText = document.createElement("p");
+
     //header - zero-padded to two digits
     headerTag.innerText = `${e.name} - ${formatSeriesAndEpisode(
       e.season,
       e.number
     )}`;
+
     //condition to view contents with images
     if (e.image !== null) {
       imageTag.src = e.image.medium;
     }
+
     //removing all p tags in text content
     let text = e["summary"];
     let newText = text.replaceAll("<p>", "");
     newText = newText.replaceAll("</p>", "");
     summaryText.innerText = newText;
+
     eachEpisodeDiv.append(headerTag, imageTag, summaryText);
     allEpisodesDiv.append(eachEpisodeDiv);
     searchEpisode.style.display = "block";
@@ -95,9 +110,11 @@ function makePageForEpisodes(episodeList) {
     selectEpisodeTag.style.display = "block";
   });
 }
+
 function makePageForShows(shows) {
   searchShow.innerText = "";
   showList.innerHTML = "";
+
   shows.forEach((show) => {
     let eachShowList = document.createElement("div");
     let eachShowHeader = document.createElement("h1");
@@ -105,18 +122,23 @@ function makePageForShows(shows) {
     const summary = document.createElement("span");
     const showImage = document.createElement("img");
     const asideDiv = document.createElement("div");
-    eachShowHeader.innerText = show.name;
+
+    eachShowHeader.innerText = show.name; 
+    eachShowHeader.className= "each-show-header"
     summary.innerHTML = show.summary;
+
     // condition to view content with images
     if (show.image !== null) {
       showImage.src = show.image.medium;
     }
+
     //aside content
     asideDiv.innerText = `Rated${
       show.rating.average
     } \n Genres: ${show.genres.join(" | ")} \n Status: ${
       show.status
     } \n Runtime: ${show.runtime}`;
+
     //class attributes
     eachShowList.className = "eachShow-container"; // note this!
     wrapDiv.className = "content-wrapper";
@@ -127,6 +149,7 @@ function makePageForShows(shows) {
     wrapDiv.append(showImage, summary, asideDiv);
     eachShowList.append(eachShowHeader, wrapDiv);
     showList.append(eachShowList);
+
     //when a show list is clicked it views all episodes
     eachShowList.addEventListener("click", (event) => {
       const selectedShowId = show.id;
@@ -142,6 +165,7 @@ function makePageForShows(shows) {
   });
   return shows;
 }
+
 function onSearchKeyUpShow(event) {
   const searchTerm = event.target.value.toLowerCase();
   const filteredShows = showsList.filter((e) => {
@@ -155,6 +179,7 @@ function onSearchKeyUpShow(event) {
   showCount.innerText = countString;
   makePageForShows(filteredShows);
 }
+
 function onSearchKeyUpEpisode(event) {
   const searchTerm = event.target.value.toLowerCase();
   searchEpisode.innerText = "";
@@ -171,6 +196,7 @@ function onSearchKeyUpEpisode(event) {
   episodeCount.innerText = countString;
   makePageForEpisodes(filteredEpisodes);
 }
+
 function getSelectedEpisodeValue(event) {
   const selectedEpisodeId = event.target.value;
   if (selectedEpisodeId === "All-Episode") {
@@ -182,6 +208,7 @@ function getSelectedEpisodeValue(event) {
     makePageForEpisodes(filteredEpisodes);
   }
 }
+
 function getSelectedShowValue(event) {
   let selectedShowId = event.target.value;
   if (selectedShowId === "All-Shows") {
@@ -196,18 +223,22 @@ function getSelectedShowValue(event) {
     });
   }
 }
+
 function goBackToAllShows() {
   backToShows.style.display = "none";
   allEpisodesDiv.innerHTML = "";
   showList.style.display = "block";
+
   selectEpisodeTag.style.display = "none";
   searchEpisode.style.display = "none";
   episodeCount.style.display = "none";
   searchShow.style.display = "block";
   showCount.style.display = "block";
 }
+
 function sendRequest(showId) {
   const urlForTheRequest = `https://api.tvmaze.com/shows/${showId}/episodes`;
+
   return fetch(urlForTheRequest)
     .then((res) => res.json())
     .then((data) => {
